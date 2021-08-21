5ba6e6611e037391cf4877b46828f37be26aaed1
Alvaro Lopez benito <alopez-b@c1r9s2.42madrid.com>
2021-08-21 17:24:56 +0200

    Libft first commit

---
+++ ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alopez-b <alopez-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/21 16:36:52 by alopez-b          #+#    #+#             */
+/*   Updated: 2021/08/21 17:13:03 by alopez-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	signe;
+	int	result;
+
+	i = 0;
+	signe = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\v')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			signe = signe * (-1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	return (result * signe);
+}
